2056a7c9c41618702f403dd3a2e71f1fb1985ecc
BentonL-source <70772728+BentonL-source@users.noreply.github.com>
2022-09-20 17:09:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var len;
 var results = '';
+var count = 1;
 
 function apiSearch() {
   var params = {
@@ -10,9 +11,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: "https://api.bing.microsoft.com/v7.0/search?" + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1fe7928c6cb3473585f02c7c9bbe0fee");
       },
       type: "GET",
     })
@@ -28,4 +29,30 @@ function apiSearch() {
     .fail(function () {
       alert("error");
     });
+
+    
 }
+
+function searchFunction() {
+    results = "";
+    document.getElementById("searchResults").style.visibility = "visible";
+    apiSearch()
+};
+
+function changeBackground() {
+    if (count % 2 == 0) {
+        document.body.style.background = 'url(https://images.unsplash.com/photo-1542451542907-6cf80ff362d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1221&q=80)'
+    } else {
+        document.body.style.background = 'url(https://images.unsplash.com/photo-1663603846233-c00ceff09aeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80)'
+    }
+
+    count++;
+};
+
+function tellTime() {
+    document.getElementById("time").style.visibility = "visible";
+    var now = new Date();
+    var currTime = now.getHours() + ":" + now.getMinutes();
+    document.getElementById("time").innerHTML = currTime;
+    
+};
